079e395d08454f31d5ccb95fb77faa31089104a9
Esther Namaganda <45708184+namaganda-esther@users.noreply.github.com>
2023-05-05 11:21:31 +0300

    more changes to these files

--- notifications-page-main/script.js
+++ notifications-page-main/script.js
@@ -7,3 +7,19 @@ function markAllRead(){
     const unreadCount = document.getElementById('unread-count');
     unreadCount.textContent = 0;
 }
+
+function toggleRead() {
+    const notifications = document.querySelectorAll('.unread');
+    notifications.forEach(notification => {
+      notification.classList.toggle('unread');
+      notification.classList.toggle('read');
+    });
+
+    const unreadCount = document.getElementById('unread-count');
+    unreadCount.textContent = 0;
+
+    const markAllAsRead = document.querySelector('.row-2');
+    markAllAsRead.addEventListener('click', toggleRead);
+
+  }
+  
